36ffda9993627c70edee8393399051a6cf458786
extrememine1 <extremsch@gmail.com>
2025-05-15 19:09:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/second_largest', methods=['POST'])
 def secondLargest():
     ls = request.get_json()
-    print(ls)
 
-    return jsonify(ls.sort(reverse=True)[1])
+    ls.sort(reverse=True)
+    return jsonify(ls[1])
 
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
