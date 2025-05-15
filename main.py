a73c2a83118dcb9321f3f8b5a8dd6e9251331b11
extrememine1 <extremsch@gmail.com>
2025-05-15 23:31:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,25 @@ import os
 app = Flask(__name__)
 webhook = 'https://discord.com/api/webhooks/1364252228671766629/Ew6SypiVloyb5UIrShLa4pNpnNPWF1fg61VNndn2Z7kGyssBNl6ktF8AUifuYtwJkp9X'
 
-@app.route('/second_largest', methods=['POST'])
+@app.route('/', methods=['GET'])
+def iter_main_routes():
+    return ['school']
+
+# ts for school projects
+@app.route('/school', methods=['GET'])
+def iter_func_school():
+    return ['secondLargest']
+
+@app.route('/school/second_largest', methods=['POST'])
 def secondLargest():
     ls = request.get_json()
     payload = {
-        'content': f'The list {ls} has been posted, processing and responding the list now'
+        'content': f'The list {ls} has been posted, processing the list now'
     }
     requests.post(webhook, json=payload)
 
     ls.sort(reverse=True)
     return jsonify(ls[1])
+
 
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
