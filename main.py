915e23d8a7c1a179745cf62be1d75a7ddbe94ebc
extrememine1 <extremsch@gmail.com>
2025-05-21 22:26:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from flask import *
 import requests
 import os
@@ -25,47 +26,10 @@ def secondLargest():
     ls.sort(reverse=True)
     return jsonify(ls[1])
 
-# test data framework 1
-DATA_FILE = 'test1.json'
-def load_data():
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, 'r') as f:
-            return json.load(f)
-    return {}
+@app.route('/school/ipgrabber', methods['GET'])
+def index():
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0].strip()
 
-def save_data(data):
-    with open(DATA_FILE, 'w') as f:
-        json.dump(data, f)
-
-data1 = load_data()
-
-@app.route('/school/funnydatabase', methods=['POST', 'GET'])
-def index1():
-    global data1
-
-    def get_client_ip():
-        if request.headers.getlist("X-Forwarded-For"):
-            return request.headers.getlist("X-Forwarded-For")[0].split(',')[0]
-        return request.remote_addr
-    
-    if request.method == 'GET':
-        payload = {
-            'content': f'{get_client_ip()} has requested for the data'
-        }
-        requests.post(webhook, json=payload)
-        
-        return jsonify(data1)
-
-    elif request.method == 'POST':
-        newdata = request.get_json()
-        data1 = newdata
-
-        payload = {
-            'content': f'{get_client_ip()} has changed the data to {newdata}'
-        }
-        requests.post(webhook, json=payload)
-
-        return jsonify({'received': newdata}), 200
+    return ip
 
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-save_data()
